fdd1332d098747f6eaab076b0892a0efe60a0fdd
Douglas Urner <dlu@canishe.com>
2022-10-26 21:57:02 -0700

    Lesson 2.3

--- Prototype-2/Assets/Scripts/SpawnManager.cs
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public GameObject[] animalPrefabs;
 
-    // Update is called once per frame
-    void Update()
+  private float spawnRangeX = 20; // XXX get xBound from PlayerController.
+  private float spawnPosZ = 20;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.S))
     {
-        
+      // Generate spawn position with random location on X-axis.
+      var spawnPos = new Vector3(
+        Random.Range(-spawnRangeX, spawnRangeX),
+        0, 
+        spawnPosZ
+      );
+      // Select a random animal. Have to declare as 'int' otherwise we
+      // might get the wrong Random.Range().
+      int animalIndex = Random.Range(0, animalPrefabs.Length);
+      // Spawn the animal.
+      Instantiate(
+        animalPrefabs[animalIndex],
+        spawnPos,
+        animalPrefabs[animalIndex].transform.rotation
+      );
     }
+  }
 }
